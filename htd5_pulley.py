11cbc2ddbb32edd874705924edb8415681be00a5
alex <kubrinsky@gmail.com>
2012-09-02 13:13:10 +0200

    htd5 script near ok

--- htd5_pulley.py
+++ htd5_pulley.py
@@ -1,28 +1,35 @@
 from math import *
 
-feed = 300
+feed = 200
 Q = 2
 pitch = 5 # htd5
-N = 44 # zub'ev
-deep = -15 # tolsina
-PD = N*pitch/pi
-OD = PD - 1.14
+N = 11 # zub'ev
+deep = -9 # tolsina
+PD = N*pitch/pi - 1.14
+OD = PD #- 1.14
 cx = 0 # coord of center
 cy = 0
 cycle = 'G83 x%.3f y%.3f q%.2f r1 z%.3f'
 toolD = 6
-zPerTurn = 1
+zPerTurn = 7
 
 #print "PD:%f OD:%f sin(90):%f" % (PD, OD, sin(pi))
 
 print "G21 F%.2f" % feed
 print "G64 P0.01"
-#print "G0 X0 Y0 Z0"
+print "m6t1"
+print "o<auto-tool-change> call"
+
+print "G21 F%.2f" % feed
 
 for i in range(N):
 	x = cx + sin(2*pi/N*i)*PD/2
 	y = cy + cos(2*pi/N*i)*PD/2
 	print cycle % (x, y, Q, deep)
+
+print "m6t2"
+print "o<auto-tool-change> call"
+print "G21 F%.2f" % feed
 
 tD = toolD + .2
 print "G0 X%.3f Y%.3f Z1" % (0, (OD + tD)/2)
@@ -37,5 +44,5 @@ while z > deep:
 tD = toolD
 print "G1 X%.3f Y%.3f" % (0, (OD + tD)/2)
 print "G2 X%.3f Y%.3f Z%.2f I0 J%.3f P1" % (0, (OD + tD)/2, z, -(OD + tD)/2)
-print "G0 z20"
+print "g53 G0 z0"
 print "M2"
